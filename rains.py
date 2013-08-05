1ea078d06aec29e29bc64a1df8348b3f1936cb5a
Luca De Feo <dfl@deagol>
2013-08-06 00:47:39 +0200

    Implemented a more efficient algorithm to find the m-th root order.
    Thanks to this algorithm, `find_root_order` is no longer the bottleneck
    for certain computations. It is indeed almost instantaneous for every
    resonable input.
    
    We've also added a lot of comments, and filled some theoretical holes.
    
    This commit merges a lot of development that happened on the branch
    `sieve`. All the history, with a lot of trial and error, is preserved in
    that branch.
    
    Squashed commit of the following:
    
    commit 8d5721989b61dfd0d44b565d532b3ef4d4c8ce7a
    Author: Luca De Feo <dfl@deagol>
    Date:   Tue Aug 6 00:26:19 2013 +0200
    
        Done implementing sieving strategy + bugfixes + comments.
    
    commit 29aeccc76be02a4e25fbdd5ec3bbb549daa7edb9
    Author: Luca De Feo <dfl@deagol>
    Date:   Sat Aug 3 03:38:24 2013 +0200
    
        Sieve now takes prime powers into account.
    
        Still needs integration, testing, and comments.
    
    commit 6f4f43c5842f7aa8b57361bed38be7f4bf0aebf8
    Author: Luca De Feo <dfl@deagol>
    Date:   Fri Aug 2 21:05:04 2013 +0200
    
        Started work on a sieve technique for m.
    
        It does not use prime powers for the moment, and lacks comments.
    
    commit cf959ae85a351a3e5857aacb2962adea966b14d9
    Author: Luca De Feo <dfl@deagol>
    Date:   Fri Aug 2 00:26:40 2013 +0200
    
        Small comment
    
    commit e7abdd25071598ee68b3632c86159efe4ce2540a
    Author: Luca De Feo <dfl@deagol>
    Date:   Thu Aug 1 22:33:19 2013 +0200
    
        Small bugfix and more comments.
    
    commit a084be98a84ccca459380cce2b54942a1b36fc73
    Author: Luca De Feo <dfl@deagol>
    Date:   Wed Jul 31 17:53:48 2013 +0200
    
        Bugfix and some comments
    
    commit 87c5e4296d8947498ef5727bce338a7d0f217a26
    Author: Luca De Feo <dfl@deagol>
    Date:   Tue Jul 16 00:42:14 2013 +0200
    
        A primitive sieving, still buggy
    
    commit 3d68107c4d6a4ec98bace6029a7cd62186746b80
    Author: Luca De Feo <dfl@deagol>
    Date:   Mon Jul 15 09:48:21 2013 +0200
    
        Still working...
    
    commit 0e41a9ef3b4f1696d69b5b0ff1f56e2afd50c2c2
    Author: Luca De Feo <dfl@deagol>
    Date:   Sun Jul 14 18:13:35 2013 -0400
    
        Started working on a sieving technique for find_root_order

--- rains.py
+++ rains.py
@@ -15,8 +15,12 @@ The algorithm for finding the generators α and β is implemented by
 '''
 
 from sage.rings.finite_rings.integer_mod_ring import Zmod
+from sage.rings.integer_ring import crt_basis
 from sage.rings.finite_rings.constructor import GF
-from sage.matrix.constructor import diagonal_matrix
+from sage.misc.cachefunc import cached_method
+from sage.sets.set import Set
+from sage.combinat.set_partition import SetPartitions
+from sage.structure.factorization import Factorization
 from sage.combinat.cartesian_product import CartesianProduct as CProd
 from sage.misc.misc import cputime, walltime
 
@@ -25,7 +29,7 @@ class FalseConjecture(Exception):
 
 def test_gens_cyclotomic(p, n):
     '''
-    Test routine for `find_gen_cyclotomic`. Constructs two random
+    Test routine for `find_gens_cyclotomic`. Constructs two random
     extensions of F_p of degree n, then calls find_gens_cyclotomic and
     tests that the returned elements have the same minimal polynomial
     over F_p, and that the polynomial has degree n.
@@ -41,7 +45,7 @@ def test_gens_cyclotomic(p, n):
 
     P = a.minpoly()
     assert(P.degree() == n)
-    assert(P == b.minpoly())
+    assert(P(b) == 0)
 
 def find_gens_cyclotomic(k1, k2):
     '''
@@ -69,9 +73,13 @@ def find_gens_cyclotomic(k1, k2):
     # Find a suitable m, see doc below
     o, G = find_root_order(p, n)
 
-    # Construct extensions of k1 and k2, if needed
+    # Construct extensions of k₁ and k₂, if needed
+    #
+    # Note: `find_unique_orbit` is horribly slow if k₁ and k₂ are not
+    # fields. Some composita tricks would be welcome.
     P = None
     if (ord > n):
+        print "Using auxiliary extension, this might be slow"
         P = GF(p**o, 'z').polynomial()
         k1, k2 = [k.extension(P) for k in (k1, k2)]
         
@@ -96,9 +104,11 @@ def find_root_order(p, n):
     Rains also requires m to be square-free, but we like to live
     dangerously.
 
-    The first two conditions imply that the m-th roots of unity
-    generate a superfield of F_{p^n}, and that the extension can be
-    defined by a polynomial with coefficients in F_p.
+    The first condition implies that the m-th roots of unity generate
+    a superfield of F_{p^n}. The second condition is not strictly
+    necessary, but it makes the algorithm much more efficient by
+    insuring that the superfield is generated over F_{p^n} by a
+    polynomial with coefficients in F_p.
 
     The third condition is needed so that the construction of
     `find_unique_orbit` applies. In his paper, Rains' gives a
@@ -108,89 +118,316 @@ def find_root_order(p, n):
 
     It is easy to see that this condition is also necessary when ℤ/m*
     is cyclic, but there are easy counterexamples when it is not
-    (e.g., take p=13, n=2, m=21, although this is a stupid example
-    because m=7 would suffice).
+    (e.g., take p=233, n=6, m=21).
 
-    Remark that, when n is a prime power, m must be a prime power, so
-    (3') is largely sufficient (just a little warning when n is a
-    power of 2). However, did I mention that we also like to do group
-    theory just for the sake of it?
-
-    The straightforward generalization of (3') is: suppose
-
-      ℤ/m* = C₁ × C₂ × ⋅⋅⋅ × C_r
-
-    and suppose that <p^o> ⊂ C₁, then (3) is satisfied if and only if
-    gcd(n, #C₁/n) = 1.
-
-    The problem is that the decomposition of ℤ/m* into cyclic groups
-    is not unique. And here I start getting sloppy.
-
-    If we know the factorization of m (and we do), using an SNF we can
-    compute a factorization of ℤ/m* into cyclic groups of orders
-
-      d₁ | d₂ | ⋅⋅⋅ | λ(m)
-
-    where λ(m) is the Carmichael function (the lcm of the orders of
-    the cyclic groups associated to the prime-power factors of m). The
-    SNF also gives a basis for such decomposition.
-
-    Unless the SNF has two entries equal to λ(m) (a very rare case,
-    but look at m=15⋅16, if you like nasty examples), there is a
-    unique group, call it C₁, of order λ(m). Suppose that the order of
-    <p> is greater than the order of any other group than C₁, then
-    necessarily <p> ⊂ C₁.
-
-    My intuition is that if these things do not happen, then a smaller
-    factor m would also have worked. However, if by running this
-    algorithm we bump into some nasty example, I will be even more
-    pleased to look at it.
-
+    The integer m and the decomposition of ℤ/m are computed by the
+    function `sieve` below, with acceptance criterion given by (1),
+    (2) and (3').
+    
     To conclude: bounds on m, hard to tell. When n is prime, m must be
     prime, and under GRH the best bound is m ∈ O(n^{2.4 + ε})
     [1]. Heuristically m ∈ O(n log n).  Pinch [2] and Rains give some
     tabulations.
 
-    Note: this algorithm is very naive and very slow (in some cases,
-    slower than `find_unique_orbit`). Any optimizations welcome.
+    Note: this algorithm loops forever if p=2 and 8|n. Rains proposes
+    two fixes in his paper, which we haven't implemented yet.
 
     [1]: D. R. Heath-Brown, Zero-free regions for Dirichlet L-functions, and
     the least prime in an arithmetic progression
     [2]: R. G. E. Pinch. Recognizing elements of finite fields.
     '''
-    m = n + 1
-    while True:
-        if m % p == 0:
-            m += 1
+    def accept(r, e, n):
+        '''
+        This function is passed down to `sieve`. It accepts only if:
+
+        (1)  the order of p in ℤ/r^e is a multiple of n;
+        (3') λ(r^e) / n is coprime to n (λ is the Carmichael function).
+
+        These two conditions are equivalent to the conditions (1)-(3)
+        above when r is prime.
+        '''
+        # Generic case
+        if r != 2 and p != r:
+            m = r**e
+            ord = (r - 1) * r**(e-1)
+            return ((ord // n.expand()).gcd(n.expand()) == 1 and          # (3')
+                    all(Zmod(m)(p)**(ord // ell) != 1 for (ell,_) in n))  # (1)
+
+        # Special treatement for ℤ/2^x
+        elif r == 2:
+            return ((e == 2 and p % 4 == 3) or
+                    (p != 2 and e - n[0][1] == 2 and     # (3')
+                     Zmod(2**e)(p)**(n.expand() // 2)))  # (1)
+
+        else:
+            return False
+    
+    # For each prime power, find the smallest multiplier k.
+    m = sieve(n, accept)
+
+    # Construct ℤ/m* as the product of the factors ℤ/f*
+    R = Zmod(prod(r for r, _, _ in m))
+    crt = map(R, crt_basis([r for r, _, _ in m]))
+    G = [(sum(crt[:i])
+          + R(-1 if r % 2 == 0 and r != 4 else (Zmod(r).unit_gens()[0]**o)) * crt[i]
+          + sum(crt[i+1:]), 
+          c)
+         for i, (r, o, c) in enumerate(m)]
+    assert(all(g**e == 1 for (g,e) in G))
+
+    ord = R(p).multiplicative_order()
+        
+    return ord // n, G
+
+
+def sieve(n, accept=None):
+    '''
+    Given n, find the smallest integer m such that there exist an
+    element of multiplicative order n in ℤ/m.
+
+    This is equivalent to the condition: for each prime power q|n,
+    there exists a prime prime power r|m such that q|φ(r). One
+    immediate consequence is that n|φ(m).
+
+    The output is a list of pairs of integers (r_i, o_i, c_i), such that
+
+    - r_i is a prime power;
+    - the r_i are pairwise coprime and m = ∏ r_i;
+    - the o_i are pairwise coprime and n = ∏ o_i;
+    - o_i · c_i = φ(r_i).
+
+    The optional parameter `accept` can be passed in order to put
+    additional constraints on m. If it is given, it must be a function
+    satisfying:
+    
+    - it takes three arguments (p, e, o), where p and e are integers
+      and o is the factorization of an integer (a `Factorization`
+      object);
+    - it returns a boolean;
+    - let s and t be coprime, accept(p,e,s·t) returns `True` iff
+      accept(p,e,s) and accept(p,e,t) also return `True`.
+
+    Then, assuming r_i = p_i^e_i, with p_i prime, the output also
+    satisfies
+
+    - accept(p_i, e_i, o_i) returns `True` for any i.
+
+    The obvious use case for `accept` is to insure that a spefic
+    element of ℤ/m has order n (or divisible by n), instead of any
+    element.
+
+
+    ## Algorithm
+    
+    The algorithm starts by factoring n into prime powers q_i. For
+    each q_i, it finds the smallest prime power r_i = p_i^e_i such
+    that q_i|r_i and `accept(p_i, e_i, q_i)` returns `True` (if
+    `accept` is provided).
+
+    At this point, the lcm of all the r_i is an acceptable value for
+    m, although not necessarily the smallest one. More generally, let
+
+      n_{1,...,s} = q₁ · q₂ ··· q_s
+
+    for some subset of the q_i (up to renumbmering), then the lcm of
+    r₁, ..., r_s is an acceptable value for n_{1,...,s}, although not
+    necessarily the smallest one. Call r_{1,...,s} this optimal value.
+    The algorithm goes on by computing the optimal values r_X for
+    larger and larger subsets X ⊂ {q_i}, until n is reached. 
+
+    This is done by testing all the possible prime powers between the
+    largest r_Y already computed for any proper subset Y ⊂ X, and the
+    smallest lcm(r_W, r_Z) for any proper partition X = W ∪ Z with
+    W ∩ Z = ∅.
+
+    For any given n' = n_{1,...,s}, the algorithm looks for primes of
+    the form k·n' + 1. If q_s = p_s^e_s is the largest prime factor of
+    n' and if
+    
+      q₁ · q₂ ··· q_{s-1} | p_s - 1
+
+    the algorithm also looks for powers of p_s. It is easy to show
+    that the algorithm needs consider no other integer.
+
+    ### Example
+
+    Let n = 60 = 4·3·5
+
+    The algorithm starts by computing
+
+      r_{4} = 1·4 + 1 = 5,
+      r_{3} = 2·3 + 1 = 7,
+      r_{5} = 2·5 + 1 = 11.
+    
+    Then it considers all possible pairs of factors. For {4,3}, one
+    possible value is 5·7 = 35. 2 divides 3-1, hence the algorithm
+    looks for primes and powers of 3 comprised between 7 and 35. It
+    finds that 13 is a suitable value, hence
+
+      r_{4,3} = 1·12 + 1 = 13,
+
+    and similarly
+
+      r_{4,5} = 25, r_{3,5} = 31.
+      
+    Finally, it considers all three factors. It is useless to test
+    prime powers below 31, because 3 and 5 must divide them. There are
+    three possible partitions of {4,3,5} into two disjoints sets:
+
+      lcm(r_{4}, r_{3,5}) = 155
+      lcm(r_{3}, r_{4,5}) = 175
+      lcm(r_{5}, r_{4,3}) = 143
+
+    hence 143 is an acceptable value, and the algorithm needs to test
+    no further. The first value tried is 1·60 + 1 = 61, and it turns
+    out it is a prime, hence the algorithm returns
+
+      [(61, 60)].
+
+    ### Complexity
+
+    The complexity is obviously polynomial in n. Heuristically, it
+    should be sublinear, the dominating step being the factorization
+    of n, but it is not so easy to prove it.
+
+    If c is the number of primary factors of n, the main loop is
+    executed 2^c times, which is clearly sublinear in n.
+
+    At each iteration, all partitions of the current subset into two
+    disjoint subsets must be considered, hence a very crude lower
+    bound for this combinatorial step is
+
+      ∑_{i=1}^{c} binom(c,i) (2^{i-1} - 1) < 3^c << e^{o(log n)}
+
+    The most expensive operation of each cycle is the primality
+    testing (and, eventually, the `accept` function). Heuristically,
+    at each iteration O(log n) primality tests are needed, each with a
+    polynomial cost in log n. As noted in `find_root_order`, the best
+    bounds under GRH give O(n^{1.4 + ε}) primality tests, instead.
+
+    Whatever the provable complexity is, this algorithm is extremely
+    fast in practice, and can handle sizes which are way beyond the
+    tractability of the other steps of Rains' algorithm.
+    '''
+    # If accept is not given, always accept
+    if accept is None:
+        accept = lambda p, e, o : True
+
+    class factorization:
+        '''
+        This class represents the factorization of an integer,
+        carrying one more piece piece of information along each
+        primary factor: factors are pairs (p^e, o), with o an
+        integer.
+        '''
+        def __init__(self, f):
+            '''
+            f must be a dictionary with entries of the form
+                p : (e, o)
+            '''
+            self.factors = f
+
+        def lcm(self, other):
+            '''
+            Compute the lcm of two factorizations. The auxiliary
+            information is multiplied together:
+
+              lcm ( (p^e, s), (p^d, t) ) = (p^max(e,d), s·t)
+            '''
+            lcm = self.factors.copy()
+            for (p,(e,o)) in other.factors.iteritems():
+                try:
+                    E, O = lcm[p]
+                    lcm[p] = (max(E,e), O*o)
+                except KeyError:
+                    lcm[p] = (e, o)
+            return factorization(lcm)
+
+        @cached_method
+        def expand(self):
+            'Return the integer represented by this factorization'
+            return prod(map(lambda (p,(e,_)) : p**e, self.factors.items()))
+
+        def __str__(self):
+            return ' * '.join('%d^%d<--%d' % (p, e, o) 
+                              for (p,(e,o)) in self.factors.iteritems())
+
+        def __repr__(self):
+            return 'factorization(%s)' % repr(self.factors)
+            
+
+    # Represent the factorization of n as a set of primary factors
+    fact = Set(list(n.factor()))
+    # This dictionary holds the values r_X for each subset of `fact`
+    optima = {}
+
+    # Main loop, execute for each subset `S` ⊂ `fact`
+    for S in fact.subsets():
+        # ignore the empty set
+        if S.is_empty():
             continue
-        R = Zmod(m)
-        ord = R(p).multiplicative_order()
 
-        # Check conditions (1) and (2)
-        if ord % n == 0 and n.gcd(ord // n) == 1:
+        # A Factorization object corresponding to S
+        Sfact = Factorization(S.list())
+        
+        # find `P` the greatest prime in `S`
+        P, E = max(S, key=lambda (p,e) : p)
+        # the product of the remaining prime powers
+        c = prod(p**e for p, e in S if p != P)
+        # a boolean, True only if it is worth testing powers of `P` in
+        # the sequel
+        powers = (P-1) % c == 0
+        # the product of all the factors of `S`
+        o = c * P**E
+        
+        # For singletons, we don't have an upper bound
+        if S.cardinality() == 1:
+            start = 1
+            end = None
+        # For larger subsets, we compute the minimum and maximum value
+        # to test
+        else:
+            parts = SetPartitions(S, 2)
+            # Start from the largest r_X already computed for any
+            # subset of `S`
+            start = max(optima[s].expand() for p in parts for s in p)
+            # Stop at the smallest lcm of any partition of `S`
+            end = min((optima[p[0]].lcm(optima[p[1]]) for p in parts),
+                      key=lambda x : x.expand())
+            
+        # We only consider primes of the form
+        #    k·o + 1
+        # and powers of `P` of the form
+        #    k·o + P^E
+        # if `powers` is true.
 
-            # Get the SNF of ℤ/m*
-            gens = R.unit_gens()
-            A = diagonal_matrix([g.multiplicative_order() for g in gens])
-            S, _, Q = A.smith_form()
-
-            # Check that my "intuition" was correct
-            if A.ncols() > 1 and ord <= S.diagonal()[-2]:
-                raise FalseConjecture('Wow! Report these: p=%d, n=%d, m=%d' % (p,n,m))
-
-            # Check condition (3).  Notice that we can divide λ(m) by
-            # n⋅o, rather than n, because we know that gcd(n,o) = 1
-            carmich = S.diagonal()[-1]
-            if n.gcd(carmich // ord) == 1:
-
-                # Get the new generators
-                gens = [(prod(g**e for g, e in zip(gens, r)), o)
-                        for r, o in zip((Q**-1).rows(), S.diagonal())]
+        # We determine the starting `k`
+        k = start // o or 1
+        while True:
+            m = k*o + 1
+            # Once the upper bound, is reached, it is used with no
+            # further test
+            if end is not None and m >= end.expand():
+                optima[S] = end
+                break
+            # Test primes
+            elif m.is_prime() and accept(m, 1, Sfact):
+                optima[S] = factorization({m:(1, o)})
+                break
+            # Test powers of `P`
+            # notice the correction for P = 2 on the second line
+            d = k*c + 1
+            if (powers and d.is_power_of(P) and
+                (P != 2 or E == 1 or k > 1) and
+                accept(P, E + d.valuation(P), Sfact)):
+                optima[S] = factorization({P:(E + d.valuation(P), o)})
+                break
                 
-                # Replace the last generator by g^n
-                gens[-1] = (lambda (g, o): (g**n, o // n))(gens[-1])
-                return ord // n, gens
-        m += 1
+            k += 1
+            
+    # the last computed r_X is the optimum for n
+    return [(p**e, o, (p-1)*p**(e-1) // o)
+            for (p,(e,o)) in optima[fact].factors.iteritems()]
 
 
 def find_unique_orbit(k, G):
