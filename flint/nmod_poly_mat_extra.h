b2ad05b99b20fe1b715a4c157e0845e2a880b0a9
Eric Schost <schost@x220t.(none)>
2014-01-17 10:51:15 -0500

    forgotten a header

---
+++ flint/nmod_poly_mat_extra.h
@@ -0,0 +1,8 @@
+#ifndef NMOD_POLY_MAT_EXTRA_H
+#define NMOD_POLY_MAT_EXTRA_H
+
+#include <flint/nmod_poly_mat.h>
+
+void nmod_poly_mat_right_tmul(nmod_poly_mat_t C, const nmod_poly_mat_t L, const nmod_poly_mat_t B, long m, long k);
+
+#endif
