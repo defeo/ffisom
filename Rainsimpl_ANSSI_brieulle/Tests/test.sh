0ec1ed523d00cfbee409c45d6d2d1b7524215154
brieulle <brieulle@users.noreply.github.com>
2014-07-07 16:15:48 +0200

    Update test.sh

--- Rainsimpl_ANSSI_brieulle/Tests/test.sh
+++ Rainsimpl_ANSSI_brieulle/Tests/test.sh
@@ -1,18 +1,70 @@
+#!/bin/bash
 N=1
 
-while [ -f "data${N}.txt"  ]
-do
-    N=$((N+1))
-done
+if [[ "$1" == 1 ]];then
+    while [ -f "data_test1_p_fixed${N}.txt"  ]
+    do
+        N=$((N+1))
+    done
 
-NAMEFILE="data${N}.txt"
+    NAMEFILE="data_test1_p_fixed${N}"
 
-echo 'Generating the data file...'
-sage print.sage $1 $2 $3 > $NAMEFILE
+    echo 'Generating the data file...'
+    sage print.sage $1 $2 $3 > ${NAMEFILE}.txt
 
-echo 'Generating the graph...'
+    echo 'Generating the graph...'
 
-echo "plot \"$NAMEFILE\" using 1:2 title 'time_m' with lines, \"$NAMEFILE\"\
-using 1:3 title 'time_E' with lines, \"$NAMEFILE\" using 1:4 title\
-'time_ordm' with lines, \"$NAMEFILE\" using 1:5 title 'time_period' with\
- lines" | gnuplot
+    echo "set term jpeg
+    set output \"${NAMEFILE}.jpg\"
+    set xlabel \"extension degree\"
+    set ylabel \"time(sec)\"
+    plot \"${NAMEFILE}.txt\" using 1:2 title 'time_m' with lines,\
+     \"${NAMEFILE}.txt\" using 1:3 title 'time_E' with lines,\
+     \"${NAMEFILE}.txt\" using 1:4 title 'time_ordm' with lines,\
+     \"${NAMEFILE}.txt\" using 1:5 title 'time_period' with lines"| gnuplot
+
+elif [[ "$1" == 2 ]];then
+    while [ -f "data_test2_p_fixed${N}.txt"  ]
+    do
+        N=$((N+1))
+    done
+
+    NAMEFILE="data_test2_p_fixed${N}.txt"
+
+    echo 'TODO'
+
+
+elif [[ "$1" == 3 ]];then
+    while [ -f "data_test3_n_fixed${N}.txt"  ]
+    do
+        N=$((N+1))
+    done
+
+    NAMEFILE="data_test3_n_fixed${N}"
+
+    echo 'Generating the data file...'
+    sage print.sage $1 $2 $3 > ${NAMEFILE}.txt
+
+    echo 'Generating the graph...'
+
+    echo "set term jpeg
+    set output \"${NAMEFILE}.jpg\"
+    set xlabel \"characteristic\"
+    set ylabel \"time(sec)\"
+    plot \"${NAMEFILE}.txt\" using 1:2 title 'time_m' with lines,\
+     \"${NAMEFILE}.txt\" using 1:3 title 'time_E' with lines,\
+     \"${NAMEFILE}.txt\" using 1:4 title 'time_ordm' with lines,\
+     \"${NAMEFILE}.txt\" using 1:5 title 'time_period' with lines"| gnuplot
+
+
+elif [[ "$1" == 4 ]];then
+    while [ -f "data_test4_n_fixed${N}.txt"  ]
+    do
+        N=$((N+1))
+    done
+
+    NAMEFILE="data_test4_n_fixed${N}"
+
+    echo 'TODO'
+
+fi
