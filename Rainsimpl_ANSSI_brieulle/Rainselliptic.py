e24d421d0fc733e2b686461701290cbdbdcc37fa
brieulle <l.brieulle@gmail.com>
2015-09-07 02:30:32 +0200

    Typos

--- Rainsimpl_ANSSI_brieulle/Rainselliptic.py
+++ Rainsimpl_ANSSI_brieulle/Rainselliptic.py
@@ -231,14 +231,14 @@ def find_unique_orbit_elliptic(E, m, case = 0, one_element = 0):
                 range(order))
             elif case == 1:
                 gen_G = Integers(m).unit_gens()[0]**n
-                order = euler_phi(m)/(2*n)
+                order = euler_phi(m)/(4*n)
         
                 return sum((XZ.ladder(P, ZZ(gen_G**i), E.a4(), E.a6())[0])**2 for i in 
                     range(order))
 
             elif case == 2:
                 gen_G = Integers(m).unit_gens()[0]**n
-                order = euler_phi(m)/(2*n)
+                order = euler_phi(m)/(6*n)
 
                 return sum((XZ.ladder(P, ZZ(gen_G**i), E.a4(), E.a6())[0])**3 for i in
                 range(order))
